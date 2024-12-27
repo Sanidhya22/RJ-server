c8ab15352bc1c550540e7adf1b381a5a0d4ff3d7
Saniddhya Nigam <sanidhyanigam99@gmail.com>
2024-12-27 13:34:40 +0530

    fix indentation

--- app.py
+++ app.py
@@ -65,44 +65,44 @@ def telegramAlertShort():
                 message = f"{tradingsymbol} \nPrice={execute_at}"
                 url = f"https://api.telegram.org/bot{config.telegram_bot_token}/sendMessage?chat_id={ema_confluence}&text={message}"
                 print(requests.get(url).json())
-                gsheet('emaconfluence', stockName)
+            gsheet('emaconfluence', stockName)
 
         elif alertName == "pivot_ema_confluence":
             for tradingsymbol, execute_at in zip(stockName, place_at):
                 message = f"{tradingsymbol} \nPrice={execute_at}"
                 url = f"https://api.telegram.org/bot{config.telegram_bot_token}/sendMessage?chat_id={pivot_ema_confluence}&text={message}"
                 # print(requests.get(url).json())
-                gsheet('pivot_ema_confluence', stockName)
+            gsheet('pivot_ema_confluence', stockName)
         elif alertName == "price_volume_analysis":
             for tradingsymbol, execute_at in zip(stockName, place_at):
                 message = f"{tradingsymbol} \nPrice={execute_at}"
                 url = f"https://api.telegram.org/bot{config.telegram_bot_token}/sendMessage?chat_id={price_volume_analysis}&text={message}"
                 print(requests.get(url).json())
-                gsheet('price_volume_analysis', stockName)
+            gsheet('price_volume_analysis', stockName)
         elif alertName == "wklyvol_emaconfluence":
             for tradingsymbol, execute_at in zip(stockName, place_at):
                 message = f"{tradingsymbol} \nPrice={execute_at}"
                 url = f"https://api.telegram.org/bot{config.telegram_bot_token}/sendMessage?chat_id={wklyvol_emaconfluence}&text={message}"
                 print(requests.get(url).json())
-                gsheet("wklyvol_emaconfluence", stockName)
+            gsheet("wklyvol_emaconfluence", stockName)
         elif alertName == "dlyvol_emaconfluence":
             for tradingsymbol, execute_at in zip(stockName, place_at):
                 message = f"{tradingsymbol} \nPrice={execute_at}"
                 url = f"https://api.telegram.org/bot{config.telegram_bot_token}/sendMessage?chat_id={dlyvol_emaconfluence}&text={message}"
                 print(requests.get(url).json())
-                gsheet("dlyvol_emaconfluence", stockName)
+            gsheet("dlyvol_emaconfluence", stockName)
         elif alertName == "wklyvol_2times_6weeks":
             for tradingsymbol, execute_at in zip(stockName, place_at):
                 message = f"{tradingsymbol} \nPrice={execute_at}"
                 url = f"https://api.telegram.org/bot{config.telegram_bot_token}/sendMessage?chat_id={wklyvol_2times_6weeks}&text={message}"
                 print(requests.get(url).json())
-                gsheet("wklyvol_2times_6weeks", stockName)
+            gsheet("wklyvol_2times_6weeks", stockName)
         elif alertName == "dlyvol_2times_7days":
             for tradingsymbol, execute_at in zip(stockName, place_at):
                 message = f"{tradingsymbol} \nPrice={execute_at}"
                 url = f"https://api.telegram.org/bot{config.telegram_bot_token}/sendMessage?chat_id={dlyvol_2times_7days}&text={message}"
                 print(requests.get(url).json())
-                gsheet("dlyvol_2times_7days", stockName)
+            gsheet("dlyvol_2times_7days", stockName)
 
     except Exception as e:
         print(e)
