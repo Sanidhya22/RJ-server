75e5ec0a47b9c86c264bef93bcd1e0c7e019b526
Saniddhya nigam <60313897+Sanidhya22@users.noreply.github.com>
2025-01-13 10:34:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,6 +122,7 @@ def gsheet(sheetName, list):
     sheetTwo = sheet.worksheet('Dashboard')
     cell = sheetOne.find(today)
     existing_date = sheetTwo.cell(2, 9).value
+    print(cell,today,existing_date)
     if cell:
         print("Column with today's date already exists.")
     if cell == None:
